be141d12140b8eb5425ce2fcf9e3723b7c9680d1
Christian Deckert <christiandeckert@Christians-iMac.local>
2014-03-22 18:30:26 +0100

    welcome #index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Hddreport::Application.routes.draw do
+  get "welcome/index"
   scope ENV['RAILS_RELATIVE_URL_ROOT'] || '/' do 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

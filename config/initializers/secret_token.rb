f2c14bd15666fa49696bd1ca9e5ed609284e3391
Christian Deckert <christiandeckert@Christians-iMac.local>
2014-03-22 18:12:53 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hddreport::Application.config.secret_key_base = '6e2306c4458b90bb67ae2995d9a83f312cad577c93d2bc2accacdb6f3f15a06022305c4905417e6c508750fe7f65a190091e9a688e50db1a676611d9f5881afd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hddreport::Application.config.session_store :cookie_store, key: '_hddreport_session'
